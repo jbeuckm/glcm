93f968b31571938ad6aa2ba9801e8cccbceb31f8
Joe Beuckman <jbeuckman@lm-jbeuckman.home>
2023-05-26 17:34:42 -0500

    ts-loader

--- webpack.config.js
+++ webpack.config.js
@@ -4,22 +4,22 @@ const isProduction = process.env.NODE_ENV == 'production'
 
 module.exports = {
   mode: isProduction ? 'production' : 'development',
-  entry: './src/GLCM.js',
+  entry: './src/GLCM.ts',
   output: {
     path: path.resolve(__dirname, 'lib'),
     filename: 'glcm.js',
     library: { name: 'GLCM', type: 'umd', export: 'GLCM' },
   },
-  // module: {
-  //   rules: [
-  //     {
-  //       test: /\.(ts|tsx)$/i,
-  //       loader: 'ts-loader',
-  //       exclude: ['/node_modules/'],
-  //     },
-  //   ],
-  // },
-  // resolve: {
-  //   extensions: ['.ts', '.js'],
-  // },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)$/i,
+        loader: 'ts-loader',
+        exclude: ['/node_modules/'],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+  },
 }
